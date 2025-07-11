1c33b14a5812be4b78680304ed5c16f48ecb8b49
Waleed Ahmad <137788903+Waleed-Ahmad-dev@users.noreply.github.com>
2025-07-11 12:48:22 +0500

    Declare routes for login

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import { signUp, verifyEmail } from '../controllers/authController';
+import { login, signUp, verifyEmail, verifyLogin } from '../controllers/authController';
 
 const router = express.Router();
 
 router.post('/signup', signUp);
 router.get('/verify-email', verifyEmail);
+router.post('/login', login)
+router.get('/verify-login', verifyLogin);
 
 export default router;
