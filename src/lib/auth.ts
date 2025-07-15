abaa8e97a87123e8f9afa8f80f0e35a2fa6ef46b
Waleed Ahmad <137788903+Waleed-Ahmad-dev@users.noreply.github.com>
2025-07-15 12:24:51 +0500

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -49,6 +49,7 @@ export const authOptions: NextAuthOptions = {
           async session({ session, user }) {
                if (session.user) {
                     session.user.id = user.id;
+                    session.user.name = user.name || null;
                }
                return session;
           },
